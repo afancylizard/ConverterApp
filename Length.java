71437f7db966db9cc06fabf50e74aa7bbef67abe
afancylizard <afancylizard@gmail.com>
2026-04-22 11:29:15 -0600

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,12 @@
+package converter;
+
+/**
+ * Represents a length measurement with a specific unit.
+ * Extends Measurement class and provides methods for converting between
+ * inches, feet, centimeters, and meters.
+ * 
+ * @author Dillan W, Aaron C, Weston P
+ */
+public class Length extends Measurement {
+
+}
