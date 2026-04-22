fe9fe40d9eace63e444f7685e73f91123ddb8ecd
afancylizard <afancylizard@gmail.com>
2026-04-22 11:35:38 -0600

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,12 @@
+package converter;
+
+/**
+ * Represents a time measurement with a specific unit.
+ * Extends Measurement class and provides methods for converting between
+ * seconds, minutes, hours, and days.
+ * 
+ * @author Aaron Carpenter, Weston Polak, Dillan Winegar
+ */
+public class Time extends Measurement {
+
+}
