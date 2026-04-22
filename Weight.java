b75e9ab0ef9d667fa0d2a3d063d49e8e24836937
afancylizard <afancylizard@gmail.com>
2026-04-22 11:33:00 -0600

    Add files via upload

---
+++ Weight.java
@@ -0,0 +1,12 @@
+package converter;
+
+/**
+ * Represents a weight measurement with a specific unit.
+ * Extends Measurement class and provided methods for converting between
+ * pounds, ounces, kilograms, and grams.
+ * 
+ * @author Aaron Carpenter, Weston Polak, Dillan Winegar
+ */
+public class Weight extends Measurement {
+
+}
