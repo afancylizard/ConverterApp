f994d311ebf3fd179f771193717fea5dfa11c3ef
afancylizard <afancylizard@gmail.com>
2026-04-22 11:24:42 -0600

    Update Measurement.java

--- Measurement.java
+++ Measurement.java
@@ -5,11 +5,14 @@ package converter;
 * All specific measurement types (Length, Weight, Time) extend this class.
 */
 public abstract class Measurement {
-	
+
+	/** The numeric value of the measurement */
 	private double value;
 
 	/**
-	 * @param value
+	 * Constructs a measurement with the specified value.
+	 *
+	 * @param value the numeric value of the measurement.
 	 */
 	public Measurement(double value) {
 		this.value = value;
